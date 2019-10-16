1768376512ddb0be43829c2ee0e89de7f730b8e2
yoko <trisetyoko333@gmail.com>
2019-10-16 09:51:57 +0700

    doc secret pos

--- apidoc/get_person.js
+++ apidoc/get_person.js
@@ -1,5 +1,5 @@
 /**
- * @api {get} /index_get Request Data User
+ * @api {get} /api/index Request Data User
  * @apiName GetUser
  * @apiGroup User
  *
@@ -10,3 +10,22 @@
  * @apiSuccess {String} address  Address of the User.
  * @apiSuccess {String} phone  Phone of the User.
  */
+
+ /**
+  * @api {post} /api/index Insert Data User
+  * @apiName PostUser
+  * @apiGroup User
+  *
+  * @apiParam {Number} id Users unique ID.
+  * @apiParam {String} name Users Required.
+  * @apiParam {String} address Users required.
+  * @apiParam {String} phone Users required.
+  *
+  * @apiSuccess {String} status Status 200.
+  * @apiSuccess {String} error Error false.
+  * @apiSuccess {String} message  Data person ditambahkan.
+  *
+  * @apiError {String} status Status 502.
+  * @apiError {String} error Error true.
+  * @apiError {String} message  Data person gagal ditambahkan.
+  */
